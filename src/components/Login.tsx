205d4908318a077902f434ac074902dd41828f5d
azhar1598 <mohammedazhar.1598@gmail.com>
2024-01-16 23:20:59 +0530

    changes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
 
 function Login() {
+    const [input, setInput] = useState({ email: '', password: '' })
+
+    const onChange = (e: any) => {
+        setInput({ ...e.target.value, input })
+    }
+
     return (
-        <div className='bg-red-500'>
+        <div className='flex flex-col align-items-center justify-center bg-red-400 w-full'>
             <h1 className='text-yellow-400'>Login</h1>
+            <div className="flex flex-col ">
+                <input type="text" placeholder="Email" name="email" onChange={onChange} value={input.email} />
+                <input type="password" placeholder="Password" name="password" onChange={onChange} value={input.password} />
+                <button>Login</button>
+            </div>
         </div>
     )
 }
